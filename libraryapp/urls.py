9f833bce97b4b7d9e61abccf50c4eca143176fee
Adrian Garmendia <dejima@gmail.com>
2020-08-05 15:20:02 -0500

    ch4

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
 
 urlpatterns = [
     path('', home, name='home'),
-    path('', book_list, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', list_librarians, name='librarians'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
 ]
--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,3 +1,4 @@
 from .books.list import book_list
 from .librarians.list import list_librarians
+from .auth.logout import logout_user
 from .home import home
