564e3e6a03df8b463a6c3b99b921445024bd39bb
Adrian Garmendia <dejima@gmail.com>
2020-08-05 10:58:21 -0500

    working on ch 3

---
+++ libraryapp/models/book.py
@@ -0,0 +1,23 @@
+from django.db import models
+from .library import Library
+from .librarian import Librarian
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=50) 
+    isbn = models.CharField(max_length=100)   
+    author = models.CharField(max_length=50)
+    year_published = models.IntegerField()
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Book")
+        verbose_name_plural = ("Books")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+                return reverse("Book_detail", kwargs={"pk": self.pk})
