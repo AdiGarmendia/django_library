f56a40086c3e3193fbaef8e8e883df40b83d0e15
Adrian Garmendia <dejima@gmail.com>
2020-08-06 11:27:22 -0500

    ch5 done

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('books/', book_list, name='books'),
     path('book/form', book_form, name='book_form'),
     path('librarians/', list_librarians, name='librarians'),
+    path('libraries/', library_list, name='libraries'),
+    path('library/form', library_form, name='library_form'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
 ]
--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -3,3 +3,5 @@ from .books.form import book_form
 from .librarians.list import list_librarians
 from .auth.logout import logout_user
 from .home import home
+from .libraries.list import library_list
+from .libraries.form import library_form
